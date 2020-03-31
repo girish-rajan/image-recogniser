b50a9ea9fe2d20e96fcbee6c894665f89d0b7d63
girish-rajan <girishrajan2009@gmail.com>
2020-03-31 15:53:22 +0530

    changing imageurl

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get('/profile/:id',(req,res)=>{profile.handleProfile(req, res, db)});
 app.put('/image', (req,res)=>{image.handleImage(req, res, db)})
 app.post('/signin', (req,res)=>{signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register',(req,res)=>{register.handleRegister(req, res, db, bcrypt)});
-app.post('/imageurl',(req,res)=>{image.handleApiCall(req, res)});
+app.post('/image',(req,res)=>{image.handleApiCall(req, res)});
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('App Started on port ${process.env.PORT}');
